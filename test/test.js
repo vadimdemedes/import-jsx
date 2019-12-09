c07dffab07186693f4d3616c95bb1ec8fa7b70a7
Jeremy Scheff <jdscheff@gmail.com>
2019-12-08 22:29:23 -0500

    Clear cache before tests run

--- test/test.js
+++ test/test.js
@@ -1,7 +1,14 @@
 'use strict';
 
+const os = require('os');
 const test = require('ava');
+const findCacheDir = require('find-cache-dir');
+const rimraf = require('rimraf');
 const importJsx = require('..');
+
+// Clear cache
+const cacheDirectory = findCacheDir({name: 'import-jsx'}) || os.tmpdir();
+rimraf.sync(cacheDirectory);
 
 const fixturePath = name => `${__dirname}/fixtures/${name}`;
 const isCached = path => Boolean(Object.keys(require.cache).includes(path + '.js'));
