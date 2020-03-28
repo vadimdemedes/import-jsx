e6471dc5adfea407c3b56201fca03f5479f5bb83
Jeremy Scheff <jdscheff@gmail.com>
2020-03-28 07:46:58 -0400

    Add a disk cache for transpiled files (#9)

---
+++ cache.js
@@ -0,0 +1,102 @@
+'use strict';
+// Based on https://github.com/babel/babel-loader/blob/15df92fafd58ec53ba88efa22de7b2cee5e65fcc/src/cache.js
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const crypto = require('crypto');
+const mkdirp = require('mkdirp');
+const findCacheDir = require('find-cache-dir');
+const transform = require('./transform');
+
+let directory;
+
+/**
+ * Build the filename for the cached file
+ *
+ * @param  {String} source  Original contents of the file to be cached
+ * @param  {Object} options Options passed to importJsx
+ * @param  {String} version Version of import-jsx
+ *
+ * @return {String}
+ */
+const filename = (source, options, version) => {
+	const hash = crypto.createHash('md4');
+	const contents = JSON.stringify({source, options, version});
+	hash.update(contents);
+
+	return hash.digest('hex') + '.js';
+};
+
+/**
+ * Handle the cache
+ *
+ * @params {String} directory
+ * @params {Object} params
+ */
+const handleCache = (directory, params) => {
+	const {
+		modulePath,
+		options,
+		source,
+		version
+	} = params;
+
+	if (!options.cache) {
+		return transform(source, options, modulePath);
+	}
+
+	const file = path.join(directory, filename(source, options, version));
+
+	try {
+		// No errors mean that the file was previously cached
+		// we just need to return it
+		return fs.readFileSync(file).toString();
+	} catch (error) {}
+
+	const fallback = directory !== os.tmpdir();
+
+	// Make sure the directory exists.
+	try {
+		mkdirp.sync(directory);
+	} catch (error) {
+		if (fallback) {
+			return handleCache(os.tmpdir(), params);
+		}
+
+		throw error;
+	}
+
+	// Otherwise just transform the file
+	// return it to the user asap and write it in cache
+	const result = transform(source, options, modulePath);
+
+	try {
+		fs.writeFileSync(file, result);
+	} catch (error) {
+		if (fallback) {
+			// Fallback to tmpdir if node_modules folder not writable
+			return handleCache(os.tmpdir(), params);
+		}
+
+		throw error;
+	}
+
+	return result;
+};
+
+/**
+ * Retrieve file from cache, or create a new one for future reads
+ *
+ * @param  {Object}   params
+ * @param  {String}   params.modulePath
+ * @param  {String}   params.source     Original contents of the file to be cached
+ * @param  {Object}   params.options    Options passed to importJsx
+ * @param  {String}   params.version    Version of import-jsx
+ */
+module.exports = params => {
+	if (!directory) {
+		directory = findCacheDir({name: 'import-jsx'}) || os.tmpdir();
+	}
+
+	return handleCache(directory, params);
+};
--- test/test.js
+++ test/test.js
@@ -1,10 +1,23 @@
 'use strict';
 
+const fs = require('fs');
 const test = require('ava');
+const findCacheDir = require('find-cache-dir');
+const rimraf = require('rimraf');
 const importJsx = require('..');
 
 const fixturePath = name => `${__dirname}/fixtures/${name}`;
 const isCached = path => Boolean(Object.keys(require.cache).includes(path + '.js'));
+
+const diskCacheDirectory = findCacheDir({name: 'import-jsx'});
+const clearDiskCache = () => {
+	rimraf.sync(diskCacheDirectory);
+};
+
+// Hacky - delete from memory cache, so it will use the disk cache
+const deleteFromMemoryCache = name => {
+	delete require.cache[`${fixturePath(name)}.js`];
+};
 
 test('throw when module id is missing', t => {
 	t.throws(() => importJsx(), TypeError, 'Expected a string');
@@ -78,4 +91,48 @@ test('parse React fragments', t => {
 	t.notThrows(() => {
 		importJsx(fixturePath('react-fragment'));
 	});
+});
+
+const diskCacheFile = `${diskCacheDirectory}/52b036e6b962bb90970618d299713122.js`;
+
+test('creates appropriate disk cache file', t => {
+	clearDiskCache();
+
+	t.false(fs.existsSync(diskCacheFile));
+
+	importJsx(fixturePath('for-cache'));
+
+	t.true(fs.existsSync(diskCacheFile));
+});
+
+test('uses disk cache', t => {
+	clearDiskCache();
+
+	deleteFromMemoryCache('for-cache');
+	const text = importJsx(fixturePath('for-cache'));
+	t.is(text, 'For testing the disk cache!');
+
+	// Alter contents in cache
+	const contents = fs.readFileSync(diskCacheFile, 'utf8');
+	fs.writeFileSync(diskCacheFile, contents.replace('For testing', 'For really testing'));
+
+	deleteFromMemoryCache('for-cache');
+	const text2 = importJsx(fixturePath('for-cache'));
+	t.is(text2, 'For really testing the disk cache!');
+});
+
+test('does not use disk cache when cache option is false', t => {
+	clearDiskCache();
+
+	deleteFromMemoryCache('for-cache');
+	const text = importJsx(fixturePath('for-cache'));
+	t.is(text, 'For testing the disk cache!');
+
+	// Alter contents in cache
+	const contents = fs.readFileSync(diskCacheFile, 'utf8');
+	fs.writeFileSync(diskCacheFile, contents.replace('For testing', 'For really testing'));
+
+	deleteFromMemoryCache('for-cache');
+	const text2 = importJsx(fixturePath('for-cache'), {cache: false});
+	t.is(text2, 'For testing the disk cache!');
 });
